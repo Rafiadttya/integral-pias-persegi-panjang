96062ec450ca4fa7a517442bfb182371a55db74f
Rafi aditya <adityarafi412@gmail.com>
2025-12-16 18:05:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Integral Pias Persegi Panjang")
 st.write("Aplikasi ini menghitung integral tentu secara numerik.")
+st.caption("Gunakan ** untuk pangkat dan * untuk perkalian. Contoh: x**2 + 2*x")
